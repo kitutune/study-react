3a14bff767f19f127fa54e3973772f8d468f77be
kitutune <p.kitutune@gmail.com>
2021-09-02 15:15:45 +0900

    ✨ コメントページで元の投稿を表示

--- src/hooks/usePost.jsx
+++ src/hooks/usePost.jsx
@@ -1,8 +1,10 @@
+import { fetcher } from 'src/utils/fetcher';
 import useSWR from 'swr';
 
 export const usePost = (id) => {
   const { data, error } = useSWR(
-    id ? `https://jsonplaceholder.typicode.com/posts/${id}` : null
+    id ? `https://jsonplaceholder.typicode.com/posts/${id}` : null,
+    fetcher
   );
 
   return {
